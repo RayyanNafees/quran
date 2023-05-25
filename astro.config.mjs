0bc87fc67ae717433ca31e11efb04c244f1056df
= <=>
2023-05-25 09:22:12 +0530

    Added Open Graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,18 @@ import vercel from "@astrojs/vercel/serverless";
 import solidJs from "@astrojs/solid-js";
 import mdx from "@astrojs/mdx";
 import UnoCSS from 'unocss/astro';
-
 import prefetch from "@astrojs/prefetch";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import dotenv from 'dotenv';
-
 dotenv.config();
+
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [solidJs(), mdx(),
-  UnoCSS({
-  // injectReset: true
-  }),
-  prefetch(), image(), sitemap()]
+  integrations: [solidJs(), mdx(), UnoCSS({
+    // injectReset: true
+  }), prefetch(), image(), sitemap()]
 });
